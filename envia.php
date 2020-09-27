d4c0e5022951c6b73ab4d6014c4570563ef25254
rafa2015 <rafa2015@users.noreply.github.com>
2020-09-27 09:46:59 -0300

    Add files via upload

---
+++ envia.php
@@ -0,0 +1,28 @@
+﻿<?php
+$remitente = $_POST['email'];
+$destinatario = 'rafarafaelrafita2009.1234a@blogger.com'; // en esta línea va el mail del destinatario.
+$asunto = 'Publicar Anuncio'; // acá se puede modificar el asunto del mail
+if (!$_POST){
+?>
+
+<?php
+}else{
+	 
+    $cuerpo = "Nombre y apellido: " . $_POST["nombre"] . "\r\n"; 
+    $cuerpo .= "Email: " . $_POST["email"] . "\r\n";
+	$cuerpo .= "Publicar Anuncio: " . $_POST["Publicar Anuncio"] . "\r\n";
+	//las líneas de arriba definen el contenido del mail. Las palabras que están dentro de $_POST[""] deben coincidir con el "name" de cada campo. 
+	// Si se agrega un campo al formulario, hay que agregarlo acá.
+
+    $headers  = "MIME-Version: 1.0\n";
+    $headers .= "Content-type: text/plain; charset=utf-8\n";
+    $headers .= "X-Priority: 3\n";
+    $headers .= "X-MSMail-Priority: Normal\n";
+    $headers .= "X-Mailer: php\n";
+    $headers .= "From: \"".$_POST['nombre']." ".$_POST['apellido']."\" <".$remitente.">\n";
+
+    mail($destinatario, $asunto, $cuerpo, $headers);
+    
+    include 'confirma.html'; //se debe crear un html que confirma el envío
+}
+?>
